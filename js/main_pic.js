85ca505245cfd8b3a23aff512ab07d43f43ead93
Soar <weekend2000119@gmail.com>
2022-10-04 16:48:58 +0900

    解决切换图片顺序问题:     使用指引指向picture1所在位置, 并随picture1同时移动

--- js/main_pic.js
+++ js/main_pic.js
@@ -1,22 +1,42 @@
 var z_index = ["3", "2", "1"];
-var pic_opacity = ["1", "0.75", "0.5"];
-var picture_id = ["p1", "p2", "p3"];
+var pic_opacity = ["1", "0.75", "0.75"];
+var picture_id = ["#p1", "#p2", "#p3"];
+var picture_width_size = ["90%","75%","75%"];
+var picture_top = ["120px", "30px", "400px"];
+var picture_left = ["120px", "30px", "400px"];
 var index = 0;
 function leftButton() {
+    var j = index+2;
     for (var i = 0;i < 3;i++) {
-        document.getElementById(picture_id[i]).style.zIndex = z_index[(index+i+1)%3];
-        document.getElementById(picture_id[i]).style.opacity = pic_opacity[(index+i+1)%3];
+        $(picture_id[i]).animate({
+            "left":picture_left[(j+i)%3],
+            "top":picture_top[(j+i)%3]
+        });
+        $(picture_id[i]).css({
+            "z-index":z_index[(j+i)%3],
+            "opacity":pic_opacity[(j+i)%3],
+            "width":picture_width_size[(j+i)%3],
+            "transition":"all 1s ease 0s"
+        });
     }
-    index = (index+1)%3;
-    console.log(index);
+    index = (index + 2)%3;
+    console.log("end index = ", index);
 }
 
 function rightButton() {
+    var j = index+1
     for (var i = 0;i < 3;i++) {
-        document.getElementById(picture_id[i]).style.zIndex = z_index[(index+i-1)%3];
-        document.getElementById(picture_id[i]).style.opacity = pic_opacity[(index+i-1)%3];
+        $(picture_id[i]).animate({
+            "left":picture_left[(j+i)%3],
+            "top":picture_top[(j+i)%3]
+        });
+        $(picture_id[i]).css({
+            "z-index":z_index[(j+i)%3],
+            "opacity":pic_opacity[(j+i)%3],
+            "width":picture_width_size[(j+i)%3],
+            "transition":"all 1s ease 0s"
+        });
     }
-    if (index == 0) index = 2;
-    else index--;
-    console.log(index);
+    index = (index+1)%3;
+    console.log("index = ",index);
 }
